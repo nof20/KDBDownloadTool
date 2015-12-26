df1a4c88a5a767bfe459371f98a5c96f6ec2547f
Nick Franklin <n.o.franklin@gmail.com>
2015-12-25 21:31:34 -0500

    Loop through long Coinbase periods; rate limiting

--- src/main/java/KDBDownloadTool/JSONAPIService.java
+++ src/main/java/KDBDownloadTool/JSONAPIService.java
@@ -3,6 +3,7 @@ package KDBDownloadTool;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -17,10 +18,18 @@ public class JSONAPIService {
 		
 	public static Object getObject(String URLString, Class<?> cl) {
 		log.info("About to poll URL: "+URLString);
+		HttpURLConnection uc;
 		Object o = null;
+		Boolean suppressIOException = false;
 		try {
 			URL url = new URL(URLString);
-			URLConnection uc = url.openConnection();
+			uc = (HttpURLConnection) url.openConnection();
+			if (uc.getResponseCode() == 429) {
+				// https://en.wikipedia.org/wiki/List_of_HTTP_status_codes -> 429 Too Many Requests (RFC 6585)
+				log.severe("Received HTTP response code 429 (Too Many Requests); backing off");
+				suppressIOException = true;
+				Thread.sleep(5000);
+			}
 			//uc.setRequestProperty("Accept-Charset", java.nio.charset.StandardCharsets.UTF_8.name());
 			//uc.setRequestProperty("Accept", "application/json");
 			//uc.setRequestProperty("User-Agent", "KDBDownloadTool (http://github.com/nof20/KDBDownloadTool)");
@@ -32,6 +41,11 @@ public class JSONAPIService {
 			log.severe(e.getMessage());
 			e.printStackTrace();
 		} catch (IOException e) {
+			if (! suppressIOException) {
+				log.severe(e.getMessage());
+				e.printStackTrace();
+			}
+		} catch (InterruptedException e) {
 			log.severe(e.getMessage());
 			e.printStackTrace();
 		}
--- src/main/java/coinbase/CoinbaseDownloader.java
+++ src/main/java/coinbase/CoinbaseDownloader.java
@@ -1,5 +1,7 @@
 package coinbase;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Properties;
@@ -11,11 +13,11 @@ import KDBDownloadTool.c;
 public class CoinbaseDownloader extends Downloader {
 
 	final static Logger log = Logger.getLogger("WikipediaDownloader");
+	static String GRANULARITY = "60"; // i.e., minutes
 
 	String baseURLString;
-	String start;
-	String end;
-	String granularity;
+	String startString;
+	String endString;
 	String finalURLString;
 	String kdbTable;
 	c c;
@@ -23,37 +25,68 @@ public class CoinbaseDownloader extends Downloader {
 	public CoinbaseDownloader(Properties props, c c) {
 		super(props, c);
 		baseURLString = props.getProperty("coinbase.baseURLString");
-		start = props.getProperty("coinbase.start");
-		end = props.getProperty("coinbase.end");
-		granularity = props.getProperty("coinbase.granularity"); // in seconds
+		startString = props.getProperty("coinbase.start");
+		endString = props.getProperty("coinbase.end");
+		// granularity = props.getProperty("coinbase.granularity");
 		kdbTable = props.getProperty("coinbase.kdbTable");
-		finalURLString = baseURLString+"?start="+start+"&end="+end+"&granularity="+granularity;
 		this.c = c;
 	}
 	
 	public void run() {
-		Double[][] results = (Double[][]) KDBDownloadTool.JSONAPIService.getObject(finalURLString, Double[][].class);
-		// Connect to KDB, add table if not already present, then save data
+		// Check KDB table exists
 		try {
-			// NB Table name is parameterized, but schema isn't, because mapping to POJO must be maintained
 			c.k("$[`"+kdbTable+" in key `.;;("+kdbTable+":([time:`datetime$()]low:`float$();high:`float$();open:`float$();close:`float$();volume:`float$());)]");
-			Iterator<Double[]> it = Arrays.asList(results).iterator();
-			while (it.hasNext()) {
-				Double[] values = it.next();
-				// Parse date
-				Long epoch = 1000*Math.round(values[0]); // Decimal SECONDS since epoch, not millis
-				java.util.Date d = new java.util.Date(epoch);
-				// Send to KDB
-				log.info("About to send "+Arrays.toString(values)+" (timestamp "+d.toString()+")");
-				Object[] data = new Object[] {d, values[1], values[2], values[3], values[4], values[5]};
-				Object[] updArray = new Object[] {".u.upd", kdbTable, data};
-				c.ks(updArray);
-			}
-		} catch (Exception e) {
-			log.severe(e.getMessage());
-			e.printStackTrace();
-		}
+		} catch (Exception e1) {
+			log.severe("Caught an error checking for KDB table:");	
+			e1.printStackTrace();
+		} 
 		
+		// Check number of points to be returned
+		Instant startInstant = Instant.parse(startString);
+		Instant endInstant = Instant.parse(endString);
+		Long minutes = Duration.between(startInstant, endInstant).toMinutes(); // Implicitly rounded down
+		log.info(minutes+" minutes time period configured");
+		Long minsElapsed = (long) 0;
+		
+		while (minsElapsed < minutes) {
+			Long sectionDuration;
+			Instant sectionStart = startInstant.plus(Duration.ofMinutes(minsElapsed));
+			if ((minutes - minsElapsed) > 199) {
+				sectionDuration = (long) 199;
+			} else {
+				sectionDuration = minutes - minsElapsed;
+			}
+			Instant sectionEnd = sectionStart.plus(Duration.ofMinutes(sectionDuration));
+			log.info("Processing "+sectionStart.toString()+" to "+sectionEnd.toString());
+			
+			finalURLString = baseURLString+"?start="+sectionStart.toString()+"&end="+sectionEnd.toString()+"&granularity="+GRANULARITY;
+			Double[][] results = (Double[][]) KDBDownloadTool.JSONAPIService.getObject(finalURLString, Double[][].class);
+			try {
+				Iterator<Double[]> it = Arrays.asList(results).iterator();
+				while (it.hasNext()) {
+					Double[] values = it.next();
+					// Parse date
+					Long epoch = 1000*Math.round(values[0]); // Decimal SECONDS since epoch, not millis
+					java.util.Date d = new java.util.Date(epoch);
+					// Send to KDB
+					//log.info("About to send "+Arrays.toString(values)+" (timestamp "+d.toString()+")");
+					Object[] data = new Object[] {d, values[1], values[2], values[3], values[4], values[5]};
+					Object[] updArray = new Object[] {".u.upd", kdbTable, data};
+					c.ks(updArray);
+				}
+				log.info(results.length+" records sent to KDB");
+			} catch (Exception e) {
+				log.severe(e.getMessage());
+				e.printStackTrace();
+			}
+			
+			minsElapsed += sectionDuration;
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				log.severe("Interrupted during sleep to avoid rate limit: ");
+				e.printStackTrace();
+			}
+		}
 	}
-
 }
